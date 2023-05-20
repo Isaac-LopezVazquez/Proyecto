7894af8b65fb82dcfb3ff90070812fa867c8d72d
Isaac Lopez <rogelio.lopez7436@alumnos.udg.mx>
2023-05-20 14:00:56 -0600

    Final

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,10 +6,10 @@
     <div class="body-wrapper">
         <!--  Header Start -->
         <header class="app-header">
-            <nav class="navbar navbar-expand-lg navbar-light">
+            <nav class="navbar navbar-expand-lg navbar-light">  
                 <!--  Poner la navegacion -->
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="/inicio"><img src="{{ asset('/img_plantilla/logos/BlackBox.jpeg') }}" alt="" width="50px"></a>
+                <a class="navbar-brand" href="/inicio"></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
